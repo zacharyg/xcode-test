40c3450c810918a57f6a2a994df42b3fd95dd6b5
Zac Guo <zacharyg@usc.edu>
2016-07-29 13:46:12 -0700

    Initial Commit

---
+++ git-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git-test
+//
+//  Created by Zachary West Guo on 7/29/16.
+//  Copyright © 2016 zechariah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
